cc9d06cb0bde3ddde8f1ee7b2c95dd5c960ac314
bimar <bimarax@gmail.com>
2023-02-10 09:58:27 +0700

    updated

--- qrcode.py
+++ qrcode.py
@@ -1,18 +1,9 @@
 import cv2
 
 def decode_qr_code(image):
-    # Read the image
     img = cv2.imread(image)
-
-    # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Initialize the QR code detector
-    qr_detector = cv2.QRCodeDetector()
-
-    # Detect QR code in the image
-    data = qr_detector.detectAndDecode(gray)
-
+    data = cv2.QRCodeDetector().detectAndDecode(gray)
     if data[0]:
         print("QR Code detected, data: ", data[0])
     else:
