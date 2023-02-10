d13d8e1046a98addfcef81f606686d735f24fe7a
bimar <bimarax@gmail.com>
2023-02-10 10:17:18 +0700

    updated

--- qrcode.py
+++ qrcode.py
@@ -4,7 +4,7 @@ def decode_qr_code(image):
     img = cv2.imread(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     data = cv2.QRCodeDetector().detectAndDecode(gray)
-    if data[0]:
+    if data:
         print("QR Code detected, data: ", data[0])
     else:
         print("QR Code not detected")
